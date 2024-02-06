6dc1bfaeb4b583a165a2321275ef78881a77b267
Dinis Vieira <dinisvieira@outlook.com>
2024-02-06 12:50:35 +0000

    Added repro solution

---
+++ MauiAppOSTheme/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiAppOSTheme
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            await Share.Default.RequestAsync(new ShareTextRequest
+            {
+                Uri = "https://learn.microsoft.com/en-us/dotnet/maui/?view=net-maui-8.0",
+                Title = "Share Maui Doc"
+            });
+        }
+    }
+}
